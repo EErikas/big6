8f2869e1af19d30d1129bd93d9e41e38a655fb84
EErikas <EErikas@github.com>
2022-01-14 12:31:45 +0200

    repo cleanup

--- scripts/cgroups-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo 'cgroup  /sys/fs/cgroup  cgroup  defaults                        0       0' >> /etc/fstab
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet cgroup_enable=memory swapaccount=1"/' /etc/default/grub
-update-grub
-mount /sys/fs/cgroup
-reboot
